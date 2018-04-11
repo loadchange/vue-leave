206d1a34d2559ad99a945fcbf01eb1fb7c03b0cf
LoadChange <soocto@gmail.com>
2018-04-11 16:17:36 +0800

    compatibility

--- src/common/util.js
+++ src/common/util.js
@@ -2,13 +2,13 @@ export function overlap(dom1, dom2) {
   let rect1 = dom1.getBoundingClientRect()
   let rect2 = dom2.getBoundingClientRect()
 
-  let x1 = rect1.x
-  let y1 = rect1.y
+  let x1 = rect1.x || rect1.left
+  let y1 = rect1.y || rect1.top
   let w1 = rect1.width
   let h1 = rect1.height
 
-  let x2 = rect2.x
-  let y2 = rect2.y
+  let x2 = rect2.x || rect2.left
+  let y2 = rect2.y || rect2.top
   let w2 = rect2.width
   let h2 = rect2.height
 
